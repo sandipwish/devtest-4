222b154bb937223b1c1214ed33da53ab66d70bee
Sandip Shaw <sandipwish@gmail.com>
2018-08-23 00:24:50 -0400

    Fixes for below bugs :- •The text box needs to gain focus when the app starts and after a new item is added. •We should not allow blank entries to be added to the task list.

--- DevTest4/DevTest4/app/app.js
+++ DevTest4/DevTest4/app/app.js
@@ -1,4 +1,43 @@
 ﻿angular.module("todoApp", ["ngRoute"])
+
+    .directive('accessibleForm', function () {
+        return {
+            link: function (scope, elem) {
+
+                // set up event handler on the form element
+                elem.on('submit', function () {
+
+                    // find the first invalid element
+                    var firstInvalid = elem[0].querySelector('.ng-invalid');
+
+                    // if we find one, set focus
+                    if (firstInvalid) {
+                        firstInvalid.focus();
+                    }
+                });
+            }
+        };
+    })
+
+    .directive('focus',
+        function ($timeout) {
+            return {
+                scope: {
+                    trigger: '@focus'
+                },
+                link: function (scope, element) {
+                    scope.$watch('trigger', function (value) {
+                        if (value === "true") {
+                            $timeout(function () {
+                                element[0].focus();
+                            });
+                        }
+                    });
+                }
+            };
+        }
+    )
+
     .config(function ($routeProvider) {
         $routeProvider
             .when("/", {
@@ -9,15 +48,22 @@
                 redirectTo: "/"
             });
     })
+
     .controller("TodoListController", function () {
         var todoList = this;
+
+        todoList.reset = function (myForm) {
+            myForm.$setPristine();
+            todoList.todoText = "";
+        }
+
         todoList.todos = [
             { text: "learn AngularJS", done: true },
             { text: "build an AngularJS app", done: false }];
 
-        todoList.addTodo = function () {
+        todoList.addTodo = function (myForm) {
             todoList.todos.push({ text: todoList.todoText, done: false });
-            todoList.todoText = "";
+            todoList.reset(myForm);
         };
 
         todoList.remaining = function () {
